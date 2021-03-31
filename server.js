013f29855ff3c5ca5bc5dea391212f439b513879
jimblu <jimmy.pezp@gmail.com>
2021-03-31 19:50:32 +0200

    initial

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
+const express = require('express');
 const app = require('express')();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server, { cors: { origin: "*"}});
 
+
+app.set('view engine', 'ejs')
+app.use(express.static('views'));
+
+
+app.get('/', (req, res) => {
+    res.render('index')
+  })
+
+
 io.on('connection', socket => {
     console.log('a user is connected')
-    socket.emit('chat-connection', '`You are connected`')
+    socket.emit('chat-connection', `You are connected`)
     socket.on('chat-message', data => {
         socket.broadcast.emit('chat-message', data)
     })
